12feb3f09af597547a382a91a5316d76675850cc
lintingying <277475478@qq.com>
2022-01-26 16:36:02 +0800

    add lib-micro and schematic-micro

---
+++ lib/projects/micro/src/lib/micro.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { MicroComponent } from './micro.component';
+
+
+
+@NgModule({
+  declarations: [MicroComponent],
+  imports: [
+  ],
+  exports: [MicroComponent]
+})
+export class MicroModule { }
